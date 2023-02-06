9f63f64aa8bd318f4f29cc4a671b40d34d8d0acf
eliszewski <liszewski31098@gmail.com>
2023-02-06 12:25:15 -0500

    feed shows society tag for each post

--- src/main/webapp/app/customUser/feed/feed.tsx
+++ src/main/webapp/app/customUser/feed/feed.tsx
@@ -6,9 +6,11 @@ import { TextFormat, Translate } from 'react-jhipster';
 import { APP_DATE_FORMAT } from 'app/config/constants';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IUserProfile } from '../../shared/dto/userProfile.model';
 
 export const Feed = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [followedProfiles, setFollowedProfiles] = useState<IUserProfile[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -18,23 +20,13 @@ export const Feed = () => {
     fetchPosts();
   }, []);
 
-  // return (
-  //   <div>
-  //     <h1>Your feed</h1>
-  //     {posts && posts.length > 0 ? (
-  //       <div>
-  //         {posts.map(post => (
-  //           <div key={post.id}>
-  //             <h3>{post.user.login}</h3>
-  //             <p>{post.content}</p>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     ) : (
-  //       <h2> No Posts Found</h2>
-  //     )}
-  //   </div>
-  // );
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const result = await axios.get('/api/profiles/followed-tags');
+      setFollowedProfiles(result.data);
+    };
+    fetchProfiles();
+  }, []);
 
   return (
     <div>
@@ -49,21 +41,24 @@ export const Feed = () => {
             </tr>
           </thead>
           <tbody>
-            {posts.map(post => (
-              <tr key={post.id}>
-                <td>{post.date ? <TextFormat type="date" value={post.date} format={APP_DATE_FORMAT} /> : null}</td>
-                <td>{post.user.login}</td>
-                <td>{post.content}</td>
-                <td>
-                  <Button tag={Link} to={`/post/${post.id}`} color="info" size="sm" data-cy="entityDetailsButton">
-                    <FontAwesomeIcon icon="eye" />{' '}
-                    <span className="d-none d-md-inline">
-                      <Translate contentKey="entity.action.view">Expand</Translate>
-                    </span>
-                  </Button>
-                </td>
-              </tr>
-            ))}
+            {posts.map(post => {
+              const userProfile = followedProfiles.find(profile => profile.login === post.user.login);
+              return (
+                <tr key={post.id}>
+                  <td>{post.date ? <TextFormat type="date" value={post.date} format={APP_DATE_FORMAT} /> : null}</td>
+                  <td>{userProfile ? userProfile.societyTag : 'N/A'}</td>
+                  <td>{post.content}</td>
+                  <td>
+                    <Button tag={Link} to={`/post/${post.id}`} color="info" size="sm" data-cy="entityDetailsButton">
+                      <FontAwesomeIcon icon="eye" />{' '}
+                      <span className="d-none d-md-inline">
+                        <Translate contentKey="entity.action.view">Expand</Translate>
+                      </span>
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </Table>
       ) : (
@@ -71,6 +66,31 @@ export const Feed = () => {
       )}
     </div>
   );
+  // return (
+  //   <table>
+  //     <thead>
+  //       <tr>
+  //         <th>Username</th>
+  //         <th>Society Tag</th>
+  //         <th>Post</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>
+  //       {posts.map(post => {
+  //         const userProfile = followedProfiles.find(
+  //           profile => profile.login === post.user.login
+  //         );
+  //         return (
+  //           <tr key={post.id}>
+  //             <td>{post.user.login}</td>
+  //             <td>{userProfile ? userProfile.societyTag : 'N/A'}</td>
+  //             <td>{post.content}</td>
+  //           </tr>
+  //         );
+  //       })}
+  //     </tbody>
+  //   </table>
+  // );
 };
 
 export default Feed;
---
+++ src/main/webapp/app/shared/dto/userProfile.model.ts
@@ -0,0 +1,4 @@
+export interface IUserProfile {
+  login: string;
+  societyTag: string;
+}
